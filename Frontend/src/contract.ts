07839fd1511c0ca38309ba03a45a88b0b2e9b537
Felipe Marin <luisf.maring0910@gmail.com>
2024-03-28 16:10:45 -0500

    codigo llamar contracto

---
+++ Frontend/src/contract.ts
@@ -0,0 +1,50 @@
+import { ethers } from 'ethers';
+import contractABI from './abi.json';
+
+
+
+const initContract = async () => {
+    try {
+        // Connect to MetaMask
+
+        if (window.ethereum) {
+
+            const provider = new ethers.BrowserProvider(window.ethereum)
+            const contractAddress = '0xAd7F77623D30ed2802F373c89E4c4B0ACAadFC23';
+            const contract = new ethers.Contract(contractAddress, contractABI, await provider.getSigner());
+
+            return contract
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+const mint = async (account, id, amount, data) => {
+    try {
+        const contract = await initContract();
+        const txResponse = await contract?.mint(account, id, amount, data);
+        await txResponse.wait(); // Esperar a que la transacción se complete
+        console.log('Mint transaction hash:', txResponse.hash);
+        return txResponse;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+const setUrl = async (new_uri) => {
+    try {
+        const contract = await initContract();
+        const txResponse = await contract?.setURI(new_uri);
+        await txResponse.wait(); // Esperar a que la transacción se complete
+        console.log('Set URI transaction hash:', txResponse.hash);
+        return txResponse;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+export {
+    mint,
+    setUrl
+}
